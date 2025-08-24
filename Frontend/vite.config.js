d05983dcd701c9ecd1281f632db896f26d64c44d
Yuvraj kumar <115492314+yuvrajkumar12@users.noreply.github.com>
2025-08-24 06:08:30 -0700

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  build: {
-    outDir: 'build' // change output folder from 'dist' to 'build'
-  }
+  build: { outDir: 'build' }
 });
